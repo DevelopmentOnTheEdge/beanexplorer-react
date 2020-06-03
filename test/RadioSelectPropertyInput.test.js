ba76af7eb93b2cef6a7e538e7282aa9b4878a70b
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2020-06-03 19:32:13 +0700

    test fix

--- test/RadioSelectPropertyInput.test.js
+++ test/RadioSelectPropertyInput.test.js
@@ -21,20 +21,20 @@ test('radioButtons change', () => {
   expect(handle.mock.calls.length).toEqual(0);
 });
 
-test('radioButtonsMultiSelect change', () => {
+test('multiSelectViaCheckboxes change', () => {
   const handle = jest.fn();
   const reloadHandle = jest.fn();
 
   const wrapper = mount(
-    <PropertyInput path={"/radioButtonsMultiSelect"} bean={bean} onChange={handle} reloadOnChange={reloadHandle}
+    <PropertyInput path={"/multiSelectViaCheckboxes"} bean={bean} onChange={handle} reloadOnChange={reloadHandle}
                    value={["chocolate", "strawberry"]}/>
   );
 
-  wrapper.find('#radioButtonsMultiSelectPropertyInput_option0').simulate('change',{ target: { checked: true } });
-  expect(reloadHandle.mock.calls[0]).toEqual(["/radioButtonsMultiSelect", ["chocolate", "strawberry", "vanilla"]]);
+  wrapper.find('#multiSelectViaCheckboxesPropertyInput_option0').simulate('change',{ target: { checked: true } });
+  expect(reloadHandle.mock.calls[0]).toEqual(["/multiSelectViaCheckboxes", ["chocolate", "strawberry", "vanilla"]]);
 
-  wrapper.find('#radioButtonsMultiSelectPropertyInput_option1').simulate('change',{ target: { checked: false } });
-  expect(reloadHandle.mock.calls[1]).toEqual(["/radioButtonsMultiSelect", ["strawberry"]]);
+  wrapper.find('#multiSelectViaCheckboxesPropertyInput_option1').simulate('change',{ target: { checked: false } });
+  expect(reloadHandle.mock.calls[1]).toEqual(["/multiSelectViaCheckboxes", ["strawberry"]]);
 
   expect(handle.mock.calls.length).toEqual(0);
 });
